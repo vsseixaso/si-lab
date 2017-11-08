9f64505c235aba57ecfc42a3db161a5ff1e0e363
vssseixaso <vinicius.oliveira@ccc.ufcg.edu.br>
2017-11-08 17:28:45 -0300

    teste fail

--- js/models/Album.js
+++ js/models/Album.js
@@ -3,13 +3,19 @@ function Album(data) {
 }
 
 Album.prototype.addMusic = function addMusic(music) {
-	var music = _.find(this.musics, function(mus) {
-		return mus.name === music.name;
-	});
+	var music = this.findMusic(music.name);
+	console.log(music);
 	if (music) {
 		return false;
 	} else {
 		this.musics.push(music);
 		return true;
 	}
+};
+
+Album.prototype.findMusic = function findMusic(name) {
+	var music = _.find(this.musics, function(mus) {
+		return mus.name === name;
+	});
+	return music;
 };
